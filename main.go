28e882db75c31fca96bfee4a2f38f16a0721bca9
Syo <155808885+syo33331@users.noreply.github.com>
2024-02-03 14:47:24 +0800

    V2

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"crypto/rand"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
@@ -22,6 +24,10 @@ func main() {
 	message, _ := reader.ReadString('\n')
 	message = strings.TrimSpace(message)
 
+	fmt.Print("Add random 3-character alphanumeric to each message? (yes/no): ")
+	addRandomStr, _ := reader.ReadString('\n')
+	addRandomStr = strings.TrimSpace(addRandomStr)
+
 	fmt.Print("Enter the number of times to send the message: ")
 	timesStr, _ := reader.ReadString('\n')
 	timesStr = strings.TrimSpace(timesStr)
@@ -31,22 +37,40 @@ func main() {
 		return
 	}
 
-	token := "Token"
+	tokens, err := readTokens("token.txt")
+	if err != nil {
+		fmt.Println("Error reading tokens:", err)
+		return
+	}
 
 	var wg sync.WaitGroup
+	tokenCount := len(tokens)
 	for i := 0; i < times; i++ {
+		modifiedMessage := message
+		if strings.ToLower(addRandomStr) == "yes" {
+			randomStr := generateRandomString(3)
+			modifiedMessage += randomStr // ランダムな文字列をメッセージの最後に追加
+		}
 		wg.Add(1)
-		go func(count int) {
-			defer wg.Done()
-			sendMessage(channelID, message, token, count)
-		}(i + 1)
+		go sendMessage(channelID, modifiedMessage, tokens[i%tokenCount], i+1, &wg)
 	}
 
 	wg.Wait()
 	fmt.Println("All messages sent")
 }
 
-func sendMessage(channelID, message, token string, count int) {
+func readTokens(filePath string) ([]string, error) {
+	fileContent, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	tokens := strings.Split(strings.TrimSpace(string(fileContent)), "\n")
+	return tokens, nil
+}
+
+func sendMessage(channelID, message, token string, count int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	url := fmt.Sprintf("https://discord.com/api/v9/channels/%s/messages", channelID)
 	content := fmt.Sprintf(`{"content": "%s"}`, message)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(content)))
@@ -66,5 +90,20 @@ func sendMessage(channelID, message, token string, count int) {
 	}
 	defer resp.Body.Close()
 
-	fmt.Printf("Message %d sent: StatusCode = %d\n", count, resp.StatusCode)
+	fmt.Printf("Message %d sent with token: StatusCode = %d\n", count, resp.StatusCode)
+}
+
+// generateRandomString は指定された長さのランダムな英数字の文字列を生成します。
+func generateRandomString(n int) string {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	bytes := make([]byte, n)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		fmt.Println("Error generating random string:", err)
+		return ""
+	}
+	for i, b := range bytes {
+		bytes[i] = letters[b%byte(len(letters))]
+	}
+	return string(bytes)
 }
